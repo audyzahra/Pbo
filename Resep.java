b993d0f7a8afa6d7c58a33f674b6868fe64603c2
AffanRifai <audyzahra21@gmail.com>
2025-09-16 01:58:22 +0700

    Resep Crabby Patty

---
+++ Resep.java
@@ -0,0 +1,16 @@
+public class Resep {
+    public static void main(String[] args) {
+        System.out.println("Resep Krabby Patty:");
+        System.out.println("----------------------");
+        System.out.println("1. Roti");
+        System.out.println("2. Daging kebo");
+        System.out.println("3. Selada");
+        System.out.println("4. Keju");
+        System.out.println("5. Tomat");
+        System.out.println("6. lengkoh acong");
+        System.out.println("7. semen 3 roda");
+        System.out.println("8. Mayonaise");
+        System.out.println("9. Kecap bango cap ketua");
+        System.out.println("10. Daging rusa");
+    }
+}
